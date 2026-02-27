4bc4c07a11cf98e5cf59b715e713a65b9af1f4bd
Daniel <ddamago0@gmail.com>
2026-02-27 14:41:39 -0500

    Bucle de operaciones

--- cajero.py
+++ cajero.py
@@ -1,1 +1,12 @@
+# 1. Configuración inicial
 saldo = 1000
+numero_operaciones = int(input("¿Cuántas operaciones desea realizar? "))
+
+# 2. Bucle de operaciones
+for i in range(numero_operaciones):
+    print(f"\n--- Operación {i+1} de {numero_operaciones} ---")
+    print("")
+    print("1 -> Consultar saldo")
+    print("2 -> Retirar dinero")
+    print("3 -> Depositar dinero")
+    print("")
